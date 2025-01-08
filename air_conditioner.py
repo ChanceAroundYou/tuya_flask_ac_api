f575a4c682b081e66921c1f5cd3857410de1d22b
Chance <chenranlin.17@gmail.com>
2025-01-08 16:02:07 +0800

    添加空调控制功能，重构代码以支持从配置文件读取参数，并更新.gitignore以排除.ini文件

---
+++ air_conditioner.py
@@ -0,0 +1,60 @@
+# air_conditioner.py
+
+from tuya_connector import TuyaOpenAPI
+
+API_ENDPOINT = "https://openapi.tuyacn.com"
+
+class AirConditioner:
+    modes = ["cool", "heat", "auto", "fan_only", "dry"]
+    fan_speeds = ["auto", "low", "medium", "high"]
+    min_temperature = 16
+    max_temperature = 30
+
+    def __init__(self, device_id, openapi: TuyaOpenAPI):
+        self.device_id = device_id
+        self.temperature = 26
+        self.power = 0
+        self.mode = 0
+        self.fan_speed = 0
+        self.openapi = openapi
+        self.get_status()
+
+    def _command(self, code, value):
+        commands = {"commands": [{"code": code, "value": value}]}
+        self.openapi.post(f"/v1.0/devices/{self.device_id}/commands", commands)
+
+    def get_status(self):
+        results = self.openapi.get(f"/v1.0/devices/{self.device_id}/status")["result"]
+        status = {
+            item["code"]: int(item["value"]) 
+            for item in results
+        }
+
+        self.power = status["power"]
+        self.mode = status["mode"]
+        self.temperature = status["temp"]
+        self.fan_speed = status["wind"]
+
+        return {
+            'power': self.power,
+            'mode': self.mode,
+            'fan_speed': self.fan_speed,
+            'temperature': self.temperature,
+        }
+
+    def set_power(self, power):
+        self._command("power", power)
+
+    def set_mode(self, mode):
+        self._command("mode", mode)
+
+    def set_fan_speed(self, fan_speed):
+        self._command("wind", fan_speed)
+
+    def set_temperature(self, temperature):
+        self._command("temp", temperature)
+        
+def air_conditioner_factory(device_id: str, access_id: str, access_key: str):
+    openapi = TuyaOpenAPI(API_ENDPOINT, access_id, access_key)
+    ac = AirConditioner(device_id, openapi)
+    return ac
--- tuya.py
+++ tuya.py
@@ -1,113 +1,33 @@
-from tuya_connector import TuyaOpenAPI
+import configparser
+
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional
-import uvicorn
 
-# ACCESS_ID = "sk3jdv43nrvq8txt73ce"
-# ACCESS_KEY = "377b9fcef2c745fc94b8b0501929ae05"
-# DEVICE_ID = "6c0acdc7f465cbd5c63p6b"
-ACCESS_ID = "7u3fhafa9vnkga4q74dy"
-ACCESS_KEY = "cfd2ea809657436db043114165fd9156"
-DEVICE_ID = "6c1b72b269c836ed16yuur"
+from air_conditioner import air_conditioner_factory
 
-API_ENDPOINT = "https://openapi.tuyacn.com"
-MQ_ENDPOINT = "wss://mqe.tuyacn.com:8285/"
+# 读取配置文件
+config = configparser.ConfigParser()
+config.read("config.ini")
 
-class AirConditioner:
-    modes = ["cool", "heat", "auto", "fan_only", "dry"]
-    fan_speeds = ["auto", "low", "medium", "high"]
-    min_temperature = 16
-    max_temperature = 30
+ACCESS_ID = config["tuya"]["ACCESS_ID"]
+ACCESS_KEY = config["tuya"]["ACCESS_KEY"]
+DEVICE_ID = config["tuya"]["DEVICE_ID"]
 
-    def __init__(self, device_id, openapi: TuyaOpenAPI):
-        self.device_id = device_id
-        self.temperature = 26
-        self.power = 0
-        self.mode = 0
-        self.fan_speed = 0
-        self.openapi = openapi
-        self.get_status()
+HOST = config["server"]["HOST"]
+PORT = int(config["server"]["PORT"])
 
-    def _command(self, code, value):
-        commands = {"commands": [{"code": code, "value": value}]}
-        self.openapi.post(f"/v1.0/devices/{self.device_id}/commands", commands)
-
-    def get_status(self):
-        status = self.openapi.get(f"/v1.0/devices/{self.device_id}/status")
-        print(status)
-        status = {each["code"]: int(each["value"]) for each in status["result"]}
-
-        self.power = status["power"]
-        self.mode = status["mode"]
-        self.temperature = status["temp"]
-        self.fan_speed = status["wind"]
-
-        status = {
-            'power': self.power,
-            'mode': self.mode,
-            'fan_speed': self.fan_speed,
-            'temperature': self.temperature,
-        }
-        return status
-
-    def set_power(self, power):
-        assert isinstance(power, int)
-        assert power in [0, 1]
-
-        self.power = power
-        
-        if power:
-            self._command("PowerOn", 'PowerOn')
-        else:
-            self._command("PowerOff", 'PowerOff')
-
-    def set_mode(self, mode):
-        if not self.power:
-            self.power = 1
-
-        assert isinstance(mode, int)
-        assert mode in range(5)
-
-        self.mode = mode
-        if mode == 4:
-            self.fan_speed = 1
-
-        self._command("M", mode)
-
-    def set_fan_speed(self, fan_speed):
-        if not self.power:
-            return
-
-        assert isinstance(fan_speed, int)
-        assert fan_speed in range(4)
-
-        self.fan_speed = fan_speed
-        self._command("F", fan_speed)
-
-    def set_temperature(self, temperature):
-        if not self.power:
-            return
-
-        assert isinstance(temperature, int)
-        assert temperature in range(self.min_temperature, self.max_temperature+1)
-
-        self.temperature = temperature
-        self._command("T", self.temperature)
-        
 
 class Control(BaseModel):
-    power: Optional[int] = None
-    mode: Optional[int] = None
-    fan_speed: Optional[int] = None
-    temperature: Optional[int] = None
+    power: int | None = None
+    mode: int | None = None
+    fan_speed: int | None = None
+    temperature: int | None = None
 
 
-openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
-openapi.connect()
-ac = AirConditioner(DEVICE_ID, openapi)
-# 使用fastapi建立一个web服务，接收请求控制空调
+ac = air_conditioner_factory(DEVICE_ID, ACCESS_ID, ACCESS_KEY)
 app = FastAPI()
+
 
 @app.post("/control")
 def control(control: Control):
@@ -122,9 +42,11 @@ def control(control: Control):
     status = ac.get_status()
     return status
 
+
 @app.get("/status")
 def status():
     return ac.get_status()
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10223, workers=1)
+    uvicorn.run(app, host=HOST, port=PORT, workers=1)
